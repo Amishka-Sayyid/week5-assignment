f1789e47577c059627ac7b213f7c880bc8244e7e
Amina Said Ibrahim <amina7585@gmail.com>
2025-01-14 14:53:16 +0000

    initial commit of the form js handlling the form data and rendering it on the page

--- client/src/form.js
+++ client/src/form.js
@@ -1,2 +1,81 @@
 import "./style.css";
 import "./mediaQueries.css";
+
+//Select the form from the DOM
+const messageForm = document.querySelector("#messageForm");
+//We need to build an event that handles the data from the form
+messageForm.addEventListener("submit", handleSubmitMessageForm);
+
+function handleSubmitMessageForm(event) {
+  event.preventDefault();
+  const formData = new FormData(messageForm);
+  const formValues = Object.fromEntries(formData);
+
+  fetch(" server url/new-data ", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({ formValues }),
+  });
+  console.log(formValues);
+  alert("message sent successfully");
+}
+
+//The same way as we fetch the POST route, we also need to fetch the GET route, so we can display the data from the database on the DOM
+//select the testimonial containerexample like below
+// const messageContainer = document.getElementById("app2");
+
+const fetchDataArray = [];
+
+async function fetchFormData() {
+  const response = await fetch("server url/ example messages", {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+    },
+  });
+  // Parsing the response
+  const data = await response.json();
+
+  //log to see the data array
+  console.log(data);
+
+  //fetch the data and put i
+  fetchDataArray.length = 0;
+  data.forEach(savingIntoThisObject);
+  function savingIntoThisObject(dataArraymessages) {
+    fetchDataArray.push(dataArraymessages);
+  }
+
+  fetchDataArray.length = 4;
+  console.log(fetchDataArray);
+
+  return fetchDataArray;
+}
+
+async function creatingElements() {
+  const SavedData = await fetchFormData();
+  console.log(SavedData);
+
+  SavedData.forEach((singledata) => {
+    const userDiv = document.createElement("div");
+    userDiv.className = "UserDiv";
+
+    userDiv.style.backgroundColor = singledata.favourite_colour;
+
+    const messageInput = document.createElement("p");
+
+    messageInput.textContent = `" ${singledata.message}"`;
+    messageInput.className = "message";
+
+    const userName = document.createElement("p");
+    userName.textContent = singledata.user_name;
+
+    userDiv.appendChild(messageInput);
+    userDiv.appendChild(userName);
+
+    messageContainer.appendChild(userDiv);
+  });
+}
+creatingElements();
